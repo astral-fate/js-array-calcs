a08a9910d7d9bf341fae29ccb1ea135807d4c4d6
Fatimah Emad El-Den <63984422+xxfatj@users.noreply.github.com>
2022-01-06 17:55:24 +0300

    Update fArrs.js

--- fArrs.js
+++ fArrs.js
@@ -8,24 +8,23 @@ function calcTip (bill) {
 
 
     if (bill >= 50 && bill <= 300){
-        return bill + 1;
+        return bill * 0.15;
     }
     else {
-       return bill + ` + 1`;
+        return bill * .20;
     }
-    //return calcTip;
+
 
 }
-calcTip();
 
 
-const bills = [3, 5, 6];
-console.log(bills);
+
+const bills = [20, 30, 15];
+console.log(`each item coasts: ${bills}`);
 
 
 //doing math on arrays
 
+const tip = [calcTip(bills[0])+bills[0], calcTip(bills[1])+bills[1], calcTip(bills[2])+bills[2]];
 
-const tip = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-
-console.log(tip);
+console.log(`the items with fee coasts: ${tip}`);
